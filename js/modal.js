033940cd4d223cccec886dea505fa7d37d2f324a
Ivan Vorona <ivorona07@gmail.com>
2025-11-29 20:52:12 +0200

    Optimization for mobile

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,14 @@
 (() => {
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector('[data-modal]'),
+    closeModalBtn: document.querySelector('[data-popup-close]'),
+    modal: document.querySelector('[data-popup]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  if (refs.closeModalBtn) {
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  }
 
   function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні
     refs.modal.classList.toggle('is-open');
   }
 })();
